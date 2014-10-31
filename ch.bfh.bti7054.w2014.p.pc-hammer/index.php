d063b57dea487201df34fbc25a568bddcd0a5520
Kai <Kai@10.10.91.11>
2014-10-31 10:49:57 +0100

    changed something

--- ch.bfh.bti7054.w2014.p.pc-hammer/index.php
+++ ch.bfh.bti7054.w2014.p.pc-hammer/index.php
@@ -1,13 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+<link rel="stylesheet" type="text/css" href="/webprogramming/ch.bfh.bti7054.w2014.p.pc-hammer/css/layout.css">
+<link rel="stylesheet" type="text/css" href="/webprogramming/ch.bfh.bti7054.w2014.p.pc-hammer/css/format.css">
+</head>
+<body>
+<div id="Header">
+<img src="/webprogramming/ch.bfh.bti7054.w2014.p.pc-hammer/img/logo.jpg"> This is the header</div >
+<div id="Content">
+<div id="Navigation">
+<ul>
 <?php
-$homo = false;
-$user = "riedo";
-if ($user == "riedo"){
-	$homo = true;
-}
 
-if ($homo == true){
-	echo "$user ist homo";
-}else{
-	echo "$user ist nicht homo";
-}
+$x = "Bob"; // assign the value "Bob" to $x
+$$x = "Alice";// assign the value "Alice" to $Bob
+echo $Bob; // as you can see!
+
 ?>
+<li>test</li>
+<li>test</li>
+</ul>
+
+</div>
+<div id="Info">this is info panel</div>
+<div id="Main">Main Content</div>
+</div>
+</body>
+</html>
