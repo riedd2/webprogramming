cc2763f78c53dfe0bf2e98404dea911319472e73
Kai <Kai@10.10.92.7>
2014-11-24 19:53:17 +0100

    pain in the ass

--- ch.bfh.bti7054.w2014.p.pc-hammer/index.php
+++ ch.bfh.bti7054.w2014.p.pc-hammer/index.php
@@ -4,61 +4,58 @@
 <?php include "Controller/LoginController.php"; ?>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
-<link rel="stylesheet" type="text/css"
-	href="/webprogramming/ch.bfh.bti7054.w2014.p.pc-hammer/css/layout.css">
-<link rel="stylesheet" type="text/css"
-	href="/webprogramming/ch.bfh.bti7054.w2014.p.pc-hammer/css/format.css">
+<link rel="stylesheet" type="text/css" href="css/layout.css">
+<link rel="stylesheet" type="text/css" href="css/format.css">
 <script src="script/jquery-2.1.1.min.js"></script>
-	</head>
+</head>
 <body>
-	<script src="script/jquery-2.1.1.min.js"></script>
+
 	<div id="Header">
-		<img
-			src="/webprogramming/ch.bfh.bti7054.w2014.p.pc-hammer/img/logo.jpg">
+		<img src="img/logo.jpg">
 		This is the header
 		<div class="login" align="right">
-		<?php 
-	
-		if (isset($_SESSION ["user"])) {
-					echo "Welcome ".$_SESSION ["user"]."<a href='Controller/logout.php'> log out</a>";
-				}
-		else{
-			  echo '<form action="index.php" method="post"><input name="user" /> User Name<br /> <input type="password"	name="pw" /> Password<br /> <input type="submit" value="Login"/></form>';
-		}
-				
-				
-				?>
+			<?php
 			
+			if (isset ( $_SESSION ["user"] )) {
+				echo "Welcome " . $_SESSION ["user"] . "<a href='Controller/logout.php'> log out</a>";
+			} else {
+				echo '<form action="index.php" method="post"><input name="user" /> User Name<br /> <input type="password"	name="pw" /> Password<br /> <input type="submit" value="Login"/></form>';
+			}
+			
+			?>	
 		</div>
 	</div>
+	
+	
 	<div id="Content">
 		<div id="Navigation">
-<!-- Accordion -->
-<h2 class="demoHeaders">Accordion</h2>
-<div id="accordion">
-	<h3>Products</h3>
-	<ul>
-	<li><a href="view/catalog.php?filter="HDD">"HDD</a></li>
-	<li>Mainboard</li>
-	</ul>
-	<h3>Account</h3>
-	<ul>
-	<li>Basked</li>
-	<li>Settings</li>
-	</ul>
-	<h3>Wizard</h3>
-	<ul>
-	<li>Start</li>
-	<li>Introduction</li>
-	</ul>
+			<!-- Accordion -->
+			<h2 class="demoHeaders">Accordion</h2>
+			<div id="accordion">
+				<h3>Products</h3>
+				<ul>
+					<li><a href="view/catalog.php?filter="HDD">"HDD</a></li>
+					<li>Mainboard</li>
+				</ul>
+				<h3>Account</h3>
+				<ul>
+					<li>Basked</li>
+					<li>Settings</li>
+				</ul>
+				<h3>Wizard</h3>
+				<ul>
+					<li>Start</li>
+					<li>Introduction</li>
+				</ul>
+			</div>
 		</div>
 		<div id="Info">this is info panel</div>
 		<div id="Main">Main Content</div>
 	</div>
+		<script src="js/jquery/external/jquery/jquery.js"></script>
+		<script src="js/jquery/jquery-ui.js"></script>
+		<script>
 	
-	<script src="js/jquery/external/jquery/jquery.js"></script>
-<script src="js/jquery/jquery-ui.js"></script>
-<script>
 
 $( "#accordion" ).accordion();
 
@@ -174,5 +171,6 @@ $( "#dialog-link, #icons li" ).hover(
 	}
 );
 </script>
+
 </body>
 </html>
