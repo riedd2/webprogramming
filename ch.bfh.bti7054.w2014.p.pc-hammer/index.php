56c41b4b17784ed980a1eff64bd5fe2a168dabff
Kai <Kai@cassandra>
2014-11-28 15:15:28 +0100

    added bootstrap and sample index2.php

--- ch.bfh.bti7054.w2014.p.pc-hammer/index.php
+++ ch.bfh.bti7054.w2014.p.pc-hammer/index.php
@@ -34,7 +34,7 @@
 			<div id="accordion">
 				<h3>Products</h3>
 				<ul>
-					<li><a href="view/catalog.php?filter="HDD">"HDD</a></li>
+					<li><a href="view/catalog.php?filter=HDD">HDD</a></li>
 					<li>Mainboard</li>
 				</ul>
 				<h3>Account</h3>
@@ -48,129 +48,10 @@
 					<li>Introduction</li>
 				</ul>
 			</div>
+
 		</div>
 		<div id="Info">this is info panel</div>
 		<div id="Main">Main Content</div>
 	</div>
-		<script src="js/jquery/external/jquery/jquery.js"></script>
-		<script src="js/jquery/jquery-ui.js"></script>
-		<script>
-	
-
-$( "#accordion" ).accordion();
-
-
-
-var availableTags = [
-	"ActionScript",
-	"AppleScript",
-	"Asp",
-	"BASIC",
-	"C",
-	"C++",
-	"Clojure",
-	"COBOL",
-	"ColdFusion",
-	"Erlang",
-	"Fortran",
-	"Groovy",
-	"Haskell",
-	"Java",
-	"JavaScript",
-	"Lisp",
-	"Perl",
-	"PHP",
-	"Python",
-	"Ruby",
-	"Scala",
-	"Scheme"
-];
-$( "#autocomplete" ).autocomplete({
-	source: availableTags
-});
-
-
-
-$( "#button" ).button();
-$( "#radioset" ).buttonset();
-
-
-
-$( "#tabs" ).tabs();
-
-
-
-$( "#dialog" ).dialog({
-	autoOpen: false,
-	width: 400,
-	buttons: [
-		{
-			text: "Ok",
-			click: function() {
-				$( this ).dialog( "close" );
-			}
-		},
-		{
-			text: "Cancel",
-			click: function() {
-				$( this ).dialog( "close" );
-			}
-		}
-	]
-});
-
-// Link to open the dialog
-$( "#dialog-link" ).click(function( event ) {
-	$( "#dialog" ).dialog( "open" );
-	event.preventDefault();
-});
-
-
-
-$( "#datepicker" ).datepicker({
-	inline: true
-});
-
-
-
-$( "#slider" ).slider({
-	range: true,
-	values: [ 17, 67 ]
-});
-
-
-
-$( "#progressbar" ).progressbar({
-	value: 20
-});
-
-
-
-$( "#spinner" ).spinner();
-
-
-
-$( "#menu" ).menu();
-
-
-
-$( "#tooltip" ).tooltip();
-
-
-
-$( "#selectmenu" ).selectmenu();
-
-
-// Hover states on the static widgets
-$( "#dialog-link, #icons li" ).hover(
-	function() {
-		$( this ).addClass( "ui-state-hover" );
-	},
-	function() {
-		$( this ).removeClass( "ui-state-hover" );
-	}
-);
-</script>
-
 </body>
 </html>
