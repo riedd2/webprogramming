eda3e940e7d041f5c7ac86f8a471e3ac39beb442
Kai <Kai@cassandra>
2014-12-28 13:22:14 +0100

    changed refresh value

--- ch.bfh.bti7054.w2014.p.pc-hammer/view/admin.php
+++ ch.bfh.bti7054.w2014.p.pc-hammer/view/admin.php
@@ -1,7 +1,7 @@
 <?php
 if(!isset($_SESSION ["admin"])){
 	echo "sorry no admin u are";
-	header('Refresh: 5; URL=/index.php',true, 303);
+	header('Refresh: 2; URL=/index.php',true, 303);
 	die();
 }
 echo "hello";
