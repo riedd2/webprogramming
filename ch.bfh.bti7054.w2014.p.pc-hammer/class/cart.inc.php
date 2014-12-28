1624a080b0f016d63e3994e3b2cb57ba47760887
David <davidriedo1@gmail.com>
2014-12-28 17:31:48 +0100

    extended chart

--- ch.bfh.bti7054.w2014.p.pc-hammer/class/cart.inc.php
+++ ch.bfh.bti7054.w2014.p.pc-hammer/class/cart.inc.php
@@ -18,14 +18,25 @@ class Cart {
 		
 	}
 	
-	public function removeItem($art, $num) {
-		if (isset ( $this->items [$art] ) && $this->items [$art] >= $num) {
-			$this->items [$art] -= $num;
-			if ($this->items [$art] == 0)
-				unset ( $this->items [$art] );
+	public function removeItem($prodId) {
+		if (isset ( $this->products [$prodId] )) {
+				unset ( $this->productQuantity [$prodId] );
+				unset ( $this->products [$prodId] );
 			return true;
-		} else
-			return false;
+		}
+	}
+	
+	public function changeQuantity($prodId, $num)
+	{
+		$quantity = $this->productQuantity[$prodId];
+		//remove product from chart
+		if($quantity + $num <= 0){
+			$this->removeItem($prodId);
+		}
+		else{
+			$this->productQuantity[$prodId] += $num;
+		}
+		
 	}
 	private function getTotalPriceforItem($prodId, $quantity)
 	{
@@ -33,16 +44,24 @@ class Cart {
 		return $price * $quantity;
 	}
 	
+	private function getButtonHtml($prodId){
+		$html =  "<button onClick='changeProduct(".$prodId.",1)'>+</button>";
+		$html = $html."<button onClick='changeProduct(".$prodId.",-1)'>-</button>";
+		$html = $html."<button onClick='changeProduct(".$prodId.",-2147483647)'>X</button>";
+		return $html;
+		
+	}
+	
 	public function display() {
 		$totalPrice = 0;
 		echo "<table border=\"1\">";
-		echo "<tr><th>Artikel</th><th>Anzahl</th><th>Preis</th></tr>";
+		echo "<tr><th>Artikel</th><th>Anzahl</th><th>Preis</th><th></th></tr>";
 		foreach ($this->products as $art ){
 			$prodId = $art->id;
 			$quantity = $this->productQuantity[$prodId];
 			$price = $this->getTotalPriceforItem($prodId, $quantity);
 			$totalPrice+= $price;
-			echo "<tr><td>".$art->name."</td><td>".$quantity."</td><td>".$price."</td></tr>";
+			echo "<tr><td>".$art->name."</td><td>".$quantity."</td><td>".$price."</td><td>".$this->getButtonHtml($prodId)."</td></tr>";
 		}
 		echo "<tr><td> <b> Total </b></td><td><td><b>".$totalPrice."</b></td></tr>";
 		echo "</table>";
--- ch.bfh.bti7054.w2014.p.pc-hammer/view/cart.php
+++ ch.bfh.bti7054.w2014.p.pc-hammer/view/cart.php
@@ -15,6 +15,21 @@ function addToBasket(product, num)
 	        }
 	    }  
 }
+
+function changeProduct(prodId,num)
+{
+	if (window.XMLHttpRequest){ xmlhttp=new XMLHttpRequest(); }else{ xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); }
+	xmlhttp.open("GET","Controller/cartHandler.php?prodId="+prodId+"&num="+num, true);
+	xmlhttp.send();
+
+	   xmlhttp.onreadystatechange=function() {
+	        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+	            var cords = document.getElementById("cart");
+	            cords.innerHTML=xmlhttp.responseText;
+	        }
+	    }  
+}
+
 </script></head>
 <body>
 	<h3>Shopping Cart:</h3>
