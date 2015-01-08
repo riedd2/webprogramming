ab0392f3cde52d8ae06b120e82af6e256d913de3
David <davidriedo1@gmail.com>
2015-01-08 15:18:57 +0100

    fix for order saving

--- ch.bfh.bti7054.w2014.p.pc-hammer/Controller/orderHandler.php
+++ ch.bfh.bti7054.w2014.p.pc-hammer/Controller/orderHandler.php
@@ -1,10 +1,21 @@
 <?php
-require_once "../class/cart.inc.php";
-
+include "/../class/dbconnector.inc.php";
 if (isset ( $_GET ["ordersToSave"] )){
 	$jsonString = $_GET ["ordersToSave"];
 	$orders = json_decode($jsonString);
+	var_dump($orders);
 	
+	return saveOrders($orders);
+
+}
+
+function saveOrders($ordersToSave){
+	$db = new dbconnector();
+	$result = $db->saveOrder($ordersToSave);
+	if ($result > 0){
+		return "success";
+	}
+	return "error";
 	
 }
 
